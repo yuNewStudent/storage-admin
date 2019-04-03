1a8345ba9efb45395b1887e4603d3b9d1cb99c81
yujian <895144737@qq.com>
2019-04-03 17:40:59 +0800

    修改了页面

--- src/pages/ProductStorage/writeorder.vue
+++ src/pages/ProductStorage/writeorder.vue
@@ -3,7 +3,7 @@
     <div class="writeorder_unit">
       <div class="writeorder_unitl">
         <span class="demonstration">供应单位:</span>
-        <el-select v-model="value" placeholder="请选择">
+        <el-select v-model="SupplyCompany" size='medium' placeholder="请选择">
           <el-option
             v-for="item in options"
             :key="item.value"
@@ -24,80 +24,93 @@
       <div class="writeorder_operationl">
         <div class="block">
           <span class="demonstration">填写日期:</span>
-          <el-date-picker v-model="value1" type="date" placeholder="选择日期"></el-date-picker>
+          <el-date-picker size='medium' v-model="writeDate" type="date" placeholder="选择日期"></el-date-picker>
         </div>
       </div>
       <div class="writeorder_operationr">
         <el-row>
-          <el-button type="primary" size="medium" @click="buttonModifythe">保存并修改</el-button>
-          <el-button type="primary" size="medium" @click="buttonsave">保存</el-button>
-          <el-button type="primary" size="medium" @click="buttonaudit">审核</el-button>
+          <el-button type="primary" size="medium" @click="handleSave">保存</el-button>
+          <el-button type="primary" size="medium" @click="handleCheck">审核</el-button>
+          <el-button type="primary" size="medium" @click="handleOut">导出</el-button>
         </el-row>
       </div>
     </div>
     <div style="clear: both;"></div>
-    <div class="writeorder_list" v-if="show===false">
-      <el-table :data="tableData" border style="width: 100%">
-        <el-table-column type="index" label="序号" width="50"></el-table-column>
-        <el-table-column prop="date" label="商品类别"></el-table-column>
-        <el-table-column prop="name" label="商品名称"></el-table-column>
-        <el-table-column prop="address" label="申请采购数量"></el-table-column>
-        <el-table-column prop="address" label="单位"></el-table-column>
-        <el-table-column prop="address" label="所在仓库"></el-table-column>
-        <el-table-column prop="address" label="经办人"></el-table-column>
-        <el-table-column prop="address" label="用途"></el-table-column>
-        <el-table-column prop="address" label="备注"></el-table-column>
+    <div class="writeorder_list">
+      <el-table
+        :data="tableData"
+        border
+        size='small'
+        style="width: 100%">
+        <el-table-column
+          type="selection"
+          width="55">
+        </el-table-column>
+        <el-table-column label="商品类别">
+          <template slot-scope="scope">
+            <el-select v-model="orders[scope.$index].goodsCategory" placeholder="请选择">
+              <el-option
+                v-for="item in options"
+                :key="item.value"
+                :label="item.label"
+                :value="item.label">
+              </el-option>
+            </el-select>
+          </template>
+        </el-table-column>
+        <el-table-column label="商品名称">
+          <template slot-scope="scope">
+            <el-input v-model="orders[scope.$index].goodsName"></el-input>
+          </template>
+        </el-table-column>
+        <el-table-column label="申请采购数量">
+          <template slot-scope="scope">
+            <el-input v-model="orders[scope.$index].goodsNum"></el-input>
+          </template>
+        </el-table-column>
+        <el-table-column label="单位">
+          <template slot-scope="scope">
+            <el-select v-model="orders[scope.$index].goodsPart" placeholder="请选择">
+              <el-option
+                v-for="item in options"
+                :key="item.value"
+                :label="item.label"
+                :value="item.label">
+              </el-option>
+            </el-select>
+          </template>
+        </el-table-column>
+        <el-table-column label="所在仓库">
+          <template slot-scope="scope">
+            <el-select v-model="orders[scope.$index].goodsStorage" placeholder="请选择">
+              <el-option
+                v-for="item in options"
+                :key="item.value"
+                :label="item.label"
+                :value="item.label">
+              </el-option>
+            </el-select>
+          </template>
+        </el-table-column>
+        <el-table-column label="经办人">
+          <template slot-scope="scope">
+            <el-input v-model="orders[scope.$index].operatorUser"></el-input>
+          </template>
+        </el-table-column>
+        <el-table-column label="用途">
+          <template slot-scope="scope">
+            <el-input v-model="orders[scope.$index].purpose"></el-input>
+          </template>
+        </el-table-column>
+        <el-table-column label="备注">
+          <template slot-scope="scope">
+            <el-input v-model="orders[scope.$index].remark"></el-input>
+          </template>
+        </el-table-column>
       </el-table>
-    </div>
-     <div class="writeorder_list" v-else>
-      <el-table :data="tableData" border style="width: 100%">
-        <el-table-column type="index" label="序号" width="50"></el-table-column>
-        <el-table-column prop="date" label="商品类别">
-          <el-select v-model="value" placeholder="请选择">
-            <el-option
-              v-for="item in options"
-              :key="item.value"
-              :label="item.label"
-              :value="item.value">
-            </el-option>
-          </el-select>
-        </el-table-column>
-        <el-table-column prop="name" label="商品名称">
-           <el-input></el-input>
-        </el-table-column>
-        <el-table-column prop="address" label="申请采购数量">
-           <el-input></el-input>
-        </el-table-column>
-        <el-table-column prop="address" label="单位">
-          <el-select v-model="value" placeholder="请选择">
-            <el-option
-              v-for="item in options"
-              :key="item.value"
-              :label="item.label"
-              :value="item.value">
-            </el-option>
-          </el-select>
-        </el-table-column>
-        <el-table-column prop="address" label="所在仓库">
-          <el-select v-model="value" placeholder="请选择">
-            <el-option
-              v-for="item in options"
-              :key="item.value"
-              :label="item.label"
-              :value="item.value">
-            </el-option>
-          </el-select>
-        </el-table-column>
-        <el-table-column prop="address" label="经办人">
-          <el-input></el-input>
-        </el-table-column>
-        <el-table-column prop="address" label="用途">
-           <el-input></el-input>
-        </el-table-column>
-        <el-table-column prop="address" label="备注">
-           <el-input></el-input>
-        </el-table-column>
-      </el-table>
+      <el-row class='add_row'>
+        <el-button type='primary' @click='addRow'>新增行</el-button>
+      </el-row>
     </div>
   </div>
 </template>
@@ -107,28 +120,7 @@ export default {
   data() {
     return {
       show: false,
-      tableData: [
-        {
-          date: "2016-05-02",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄"
-        },
-        {
-          date: "2016-05-04",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1517 弄"
-        },
-        {
-          date: "2016-05-01",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1519 弄"
-        },
-        {
-          date: "2016-05-03",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1516 弄"
-        }
-      ],
+      tableData: [{}],
       options: [
         {
           value: "选项1",
@@ -151,34 +143,54 @@ export default {
           label: "北京经贸技校公司"
         }
       ],
-      value: "",
-      value1: ""
+      writeDate: '',
+      SupplyCompany: '',
+      orders: [
+        {
+          goodsCategory: '',
+          goodsName: '',
+          goodsNum: '',
+          goodsPart: '',
+          goodsStorage: '',
+          operatorUser: '',
+          purpose: '',
+          remark: ''
+        }
+      ]
     };
   },
   components: {},
   methods:{
-    buttonModifythe:function(){
-      if(this.show==false){
-        this.show=true;
-      }else{
-        this.show=false;
-      }
-      console.log(this.show)
+    handleSave () {
+      console.log(this.orders)
     },
-    buttonsave:function(){
+    handleOut () {
 
     },
-    buttonaudit:function(){
+    handleCheck () {
 
+    },
+    addRow () {
+      this.tableData.push({})
+      this.orders.push(
+        {
+          goodsCategory: '',
+          goodsName: '',
+          goodsNum: '',
+          goodsPart: '',
+          goodsStorage: '',
+          operatorUser: '',
+          purpose: '',
+          remark: ''
+        }
+      )
     }
   }
 };
 </script>
-<style>
+<style lang="scss" scoped>
 .writeorder {
-  padding: 20px 20px;
-}
-.writeorder_unit {
+  margin: 20px 30px;
 }
 .writeorder_unitl {
   float: left;
@@ -206,5 +218,9 @@ export default {
 }
 .writeorder_list {
   margin-top: 20px;
+  .add_row {
+    text-align: right;
+    margin-top: 20px;
+  }
 }
 </style>
